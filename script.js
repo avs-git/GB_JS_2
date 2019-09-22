1bb8f3886fb36b3bc9e8da6c3eed28be9ed77f8d
avs-git <schmidtav@yandex.ru>
2019-09-22 15:21:40 +0300

    lesson1 done

--- script.js
+++ script.js
@@ -3,16 +3,22 @@ const goods = [
     { title: 'Socks', price: 50 },
     { title: 'Jacket', price: 350 },
     { title: 'Shoes', price: 250 },
-  ];
-  
-  const renderGoodsItem = (title, price) => {
-    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
-  };
-  
-  const renderGoodsList = (list) => {
-    let asd = list.map(item => console.log(item));
-    let goodsList = list.map(({ title, price }) => renderGoodsItem(title, price));
-    document.querySelector('.goods-list').innerHTML = goodsList;
-  }
+    {},
+];
 
-  renderGoodsList(goods);
+const renderGoodsItem = (title, price) => {
+    return `<div class="goods-item"><h3>${title}</h3><p>${price}</p></div>`;
+};
+
+const renderGoodsList = (list) => {
+    const goodsContainer = document.querySelector('.goods-list');
+
+    list.map(
+        ({
+            title = 'неизвестный товар',
+            price = 0
+        }) => goodsContainer.innerHTML += renderGoodsItem(title, price));
+
+}
+
+renderGoodsList(goods);
