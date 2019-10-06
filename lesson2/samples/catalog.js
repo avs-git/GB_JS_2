4e6f07d477413ca13d5631c373dbb06644281297
avs-git <schmidtav@yandex.ru>
2019-10-07 01:03:44 +0300

    fetching goods, connect json to previous realization of shop cart

--- lesson2/samples/catalog.js
+++ lesson2/samples/catalog.js
@@ -1,34 +1,83 @@
+function sendRequest(url) {
+  return new Promise((resolve, reject) => {
+    const xhr = new XMLHttpRequest();
+    xhr.open('GET', url);
+
+    xhr.onreadystatechange = () => {
+      if (xhr.readyState === XMLHttpRequest.DONE) {
+        if (xhr.status !== 200) {
+          reject();
+        }
+        const response = JSON.parse(xhr.responseText);
+
+        resolve(response);
+      }
+    };
+    xhr.send();
+  });
+}
+
+function makeSimpleDom(tagName, innerHTML, className) {
+  let tempElement = document.createElement(tagName);
+  tempElement.className = className;
+  tempElement.innerHTML = innerHTML;
+  return tempElement;
+}
+
 class ItemsList {
   constructor() {
     this.items = [];
   }
 
   fetchItems() {
-    this.items = [
-      { title: 'Компьютерная мышь', price: 400 },
-      { title: 'Жесткий диск SSD 1Tb', price: 10000 },
-      { title: 'Материнская плата', price: 4000 },
-      { title: 'Видео-карта', price: 15000 },
-    ];
+    return sendRequest('/goods')
+      .then((items) => {
+        return this.items = items;
+      });
+  }
+
+  total() {
+    return this.items.reduce((acc, item) => acc + item.price, 0);
   }
 
   render() {
-    return this.items.map((item) => new Item(item.title, item.price).render()).join('');
+    return this.items.map((item) => new Item(item.id, item.title, item.price).render());
   }
+
 }
 
 class Item {
-  constructor(title, price) {
+  constructor(id, title, price) {
+    this.id = id;
     this.price = price;
     this.title = title;
+    this.cartItem = new CartItem(this.id, this.title, this.price, cart);
   }
 
   render() {
-    return `<div class="item"><h3>${this.title}</h3><p>${this.price}</p></div>`
+    const $item = makeSimpleDom(
+        'div',
+        `<h3>${this.title}</h3><p>${this.price}</p>`,
+        'item'
+    );
+    const $button = makeSimpleDom('button', 'добавить в корзину');
+    $button.addEventListener('click', () => {
+      cart.addToCart(this.cartItem);
+    });
+    $item.appendChild($button);
+    return $item;
   }
 }
 
 const items = new ItemsList();
-items.fetchItems();
+items.fetchItems().then(() => {
+  items.render().forEach(item => {
+    document.querySelector('.catalog').appendChild(item);
+  });
+});
 
-document.querySelector('.catalog').innerHTML = items.render();
+document.querySelectorAll('.buyButton').forEach((button) => {
+  button.addEventListener('click', (event) => {
+
+  })
+});
